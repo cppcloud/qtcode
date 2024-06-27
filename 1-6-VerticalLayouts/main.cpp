d445b65c713e520acf41a48ec495afa1e42fa8c8
cppcloud <admin@cplusplus.cloud>
2024-06-27 22:17:09 +0800

    VerticalLayouts

---
+++ 1-6-VerticalLayouts/main.cpp
@@ -0,0 +1,15 @@
+#include "dialog.h"
+
+#include <QApplication>
+
+/*
+ Vertical layout
+ */
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Dialog w;
+    w.show();
+    return a.exec();
+}
