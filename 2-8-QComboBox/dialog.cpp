a23c0d04ad89e267d60a41241befb332d132d447
cppcloud <admin@cplusplus.cloud>
2024-06-27 23:00:53 +0800

    QComboBox

---
+++ 2-8-QComboBox/dialog.cpp
@@ -0,0 +1,81 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+
+Dialog::Dialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+
+    init();
+    load();
+
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+
+void Dialog::on_comboBox_currentIndexChanged(int index)
+{
+    ui->lblSelected->setText( QString::number(index) + " = " + ui->comboBox->currentText());
+}
+
+void Dialog::on_btnSave_clicked()
+{
+    QSettings settings("MySoft", "MyApp");
+    settings.setValue("settings", ui->comboBox->currentIndex());
+
+    QMessageBox::information(this,"Saved","Selection saved, please close and re-open the application");
+}
+
+void Dialog::init()
+{
+    ui->comboBox->clear();
+
+    for (int i = 0;i < 10;i++)
+    {
+        ui->comboBox->addItem("Item number " + QString::number(i));
+    }
+}
+
+void Dialog::load()
+{
+    QSettings settings("MySoft", "MyApp");
+    QVariant value = settings.value("settings",0);
+
+    bool ok;
+    int index = value.toInt(&ok);
+    if(!ok)
+    {
+        QMessageBox::critical(this,"Loading Error","Error loading selection!");
+        return;
+    }
+
+    if(index < ui->comboBox->count())
+    {
+        ui->comboBox->setCurrentIndex(index);
+    }
+    else
+    {
+        ui->comboBox->setCurrentIndex(0);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
