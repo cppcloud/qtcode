5737bf22c7032ab8cc6958e7884b050a34517c11
cppcloud <admin@cplusplus.cloud>
2024-06-28 12:51:53 +0800

    QSpinBox

---
+++ 3-4-QSpinBox/dialog.cpp
@@ -0,0 +1,82 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+
+Dialog::Dialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+    load();
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+
+void Dialog::on_buttonBox_accepted()
+{
+    save();
+    accept();
+}
+
+void Dialog::on_buttonBox_rejected()
+{
+    reject();
+}
+
+void Dialog::load()
+{
+    ui->txtName->setText("");
+    ui->sbAge->setValue(0);
+    ui->sbQty->setValue(0);
+
+    QFile file("file.dat");
+
+    if(!file.exists()) return;
+
+    if(!file.open(QIODevice::ReadOnly))
+    {
+       QMessageBox::critical(this,"Error", file.errorString());
+       return;
+    }
+
+    QDataStream stream(&file);
+
+    QString name;
+    int age;
+    double qty;
+
+    stream >> name;
+    stream >> age;
+    stream >> qty;
+    ui->txtName->setText(name);
+    ui->sbAge->setValue(age);
+    ui->sbQty->setValue(qty);
+
+    file.close();
+}
+
+void Dialog::save()
+{
+    QFile file("file.dat");
+    if(!file.open(QIODevice::WriteOnly))
+    {
+       QMessageBox::critical(this,"Error", file.errorString());
+       return;
+    }
+
+    QDataStream stream(&file);
+    stream << ui->txtName->text();
+    stream << ui->sbAge->value();
+    stream  << ui->sbQty->value();
+
+    file.close();
+    QMessageBox::information(this,"Saved","We have saved the file!");
+}
+
+
+
+
+
