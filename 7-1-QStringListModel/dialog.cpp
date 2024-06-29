678dae26c9b34fac5d7a82efdaec71fbc90937ee
cppcloud <admin@cplusplus.cloud>
2024-06-29 21:59:56 +0800

    QStringListModel

---
+++ 7-1-QStringListModel/dialog.cpp
@@ -0,0 +1,88 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+
+Dialog::Dialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+    init();
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+void Dialog::on_buttonBox_accepted()
+{
+    accept();
+}
+
+void Dialog::on_buttonBox_rejected()
+{
+    reject();
+}
+
+void Dialog::add()
+{
+    QString value = QInputDialog::getText(this,"Add Item","Enter the item to add");
+    if(value.isEmpty()) return;
+
+    //Use the inherited functions of QAbstractModel
+    if(model.insertRow(model.rowCount()))
+    {
+        QModelIndex index = model.index((model.rowCount() -1), 0);
+        model.setData(index,value);
+    }
+}
+
+void Dialog::remove()
+{
+    QModelIndex index = ui->listView->currentIndex();
+    model.removeRow(index.row());
+}
+
+void Dialog::init()
+{
+    //Set up the data
+    list.append("Cat");
+    list.append("Dog");
+    list.append("Bird");
+    list.append("Fish");
+
+    //Setup the model, using the data
+    model.setStringList(list);
+
+    //Setup the view
+    ui->listView->setModel(&model);
+
+    //Build our UI
+    QPushButton *btnAdd = new QPushButton("Add",this);
+    QPushButton *btnRemove = new QPushButton("Remove",this);
+
+    connect(btnAdd,&QPushButton::clicked,this,&Dialog::add);
+    connect(btnRemove,&QPushButton::clicked,this,&Dialog::remove);
+
+    ui->buttonBox->addButton(btnAdd,QDialogButtonBox::ButtonRole::ActionRole);
+    ui->buttonBox->addButton(btnRemove,QDialogButtonBox::ButtonRole::ActionRole);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
